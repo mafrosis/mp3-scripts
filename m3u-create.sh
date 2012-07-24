326fe602fc6eff3992cc3ba29ae89d2807b54785
mafro <github@mafro.net>
2012-07-25 08:13:21 +1000

    simplified m3u files

--- m3u-create.sh
+++ m3u-create.sh
@@ -36,24 +36,13 @@ then
 	fi
 fi
 
-# create new file
-echo "#EXTM3U" > "$DIR.m3u"
-
 # set Input Field Separator to newline
 IFS="$(echo -en "\n\r")"
 
 for FILENAME in $(find $PWD -maxdepth 1 -name "*.mp3" | sort -n)
 do
 	NAME=${FILENAME##*/}
-
-	# extract the ID3v2 title tag
-	TITLE=$(id3v2 -l "$FILENAME" | awk '/TIT2/ {sub("TIT2 \(Title/songname/content description\): ",""); print;}')
-
-	# get the length of mp3 file
-	LEN=$(mp3info -p "%S" "$FILENAME")
-
-	echo "#EXTINF:$LEN,$TITLE" >> "$DIR.m3u"
 	echo "$NAME" >> "$DIR.m3u"
-	echo "Created $DIR.m3u"
 done
 
+echo "Created $DIR.m3u"
